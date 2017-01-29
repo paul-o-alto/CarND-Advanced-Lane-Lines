0ed0b05248fe77813c328c6d569130206a66d1e0
Paul Galloway <galloway.paul.f@gmail.com>
2017-01-28 20:07:29 -0800

    Added some new helper code for line fitting

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,8 @@ import cv2
 import pickle
 import matplotlib.pyplot as plt
 
+DEBUG = True
+
 # Define a function that takes an image, gradient orientation,
 # and threshold min / max values.
 def abs_sobel_thresh(img, sobel_kernel=3, orient='x', thresh_min=0, thresh_max=255):
@@ -115,7 +117,7 @@ def project_onto(unwarped, warped, Minv,
 
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(warped).astype(np.uint8)
-    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))*255
 
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left  = np.array([ np.transpose(np.vstack([left_fitx , yvals]))])
@@ -136,9 +138,75 @@ def project_onto(unwarped, warped, Minv,
 
     return result
 
+def sliding_window_search(binary_warped, leftx_base, rightx_base):
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binimg.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binimg.shape[0] - (window+1)*window_size
+        win_y_high = binimg.shape[0] - window*window_size
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = (((nonzeroy >= win_y_low) 
+                         & (nonzeroy < win_y_high) 
+                         & (nonzerox >= win_xleft_low) 
+                         & (nonzerox < win_xleft_high)).nonzero()[0])
+        good_right_inds = (((nonzeroy >= win_y_low) 
+                          & (nonzeroy < win_y_high) 
+                          & (nonzerox >= win_xright_low) 
+                          & (nonzerox < win_xright_high)).nonzero()[0])
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+def optimized_search(binary_warped, left_fit, right_fit):
+
+    # Assume you now have a new warped binary image 
+    # from the next frame of video (also called "binary_warped")
+    # It's now much easier to find line pixels!
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds  = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) 
+                     & (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin))) 
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin)) 
+                     & (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))  
+
+    return left_lane_inds, right_lane_inds
+
 def pipeline(cal_vars, src_img):
 
-    # TODO: LOOK BACK AT PROJECT 1 for more useful code
     ret, mtx, dist, rvecs, tvecs = cal_vars
 
     #plt.imshow(src_img); plt.show()
@@ -202,43 +270,53 @@ def pipeline(cal_vars, src_img):
 
 
     print("Detecting lane pixels and fitting to find lane boundary.")
-    yvals = np.sum(combined[combined.shape[0]/2:,:], axis=0)
-    plt.plot(yvals)
-    plt.show()
+    histogram = np.sum(combined[combined.shape[0]/2:,:], axis=0)
+    if DEBUG: plt.plot(histogram); plt.show()
 
-    # What if one lane is detected much more strongly?
-    search_y = np.copy(yvals)
-    leftx  = np.argmax(search_y)
-    rightx = np.argmax(np.flipud(search_y))
-    if rightx == leftx:
-        search_y[rightx] = 0
-        rightx = np.argmax(np.flipud(search_y)) 
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    #search_y = np.copy(yvals)
+    #leftx  = np.argmax(search_y)
+    #rightx = np.argmax(np.flipud(search_y))
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    #if rightx == leftx:
+    #    search_y[rightx] = 0
+    #    rightx = np.argmax(np.flipud(search_y)) 
+    if not optimize:
+        left_lane_inds, right_lane_inds = sliding_window_search(combined, leftx_base, rightx_base)
+    else:
+        left_lane_inds, right_lane_inds = optimized_search(binary_warped, left_fit, right_fit)
 
-    search_range = 100
-    print("Determine curvature of the lane and vehicle position")
-    print("with respect to center.") # TODO
-    # Fit a second order polynomial to each fake lane line
-    print(yvals, leftx, rightx)
-    leftx_s = np.linspace(leftx-search_range, leftx+search_range, num=len(yvals))
-    left_fit = np.polyfit(yvals, leftx_s, 2)
-    left_fitx = left_fit[0]*yvals**2 + left_fit[1]*yvals + left_fit[2]
-    rightx_s = np.linspace(rightx-search_range, rightx+search_range, num=len(yvals))
-    right_fit = np.polyfit(yvals, rightx_s, 2)
-    right_fitx = right_fit[0]*yvals**2 + right_fit[1]*yvals + right_fit[2]
-    # Plot up the data # FOR DEBUG
-    plt.plot(leftx_s, yvals, 'o', color='red')
-    plt.plot(rightx_s, yvals, 'o', color='blue')
-    plt.xlim(0, 1280)
-    plt.ylim(0, 720)
-    plt.plot(left_fitx, yvals, color='yellow', linewidth=10)
-    plt.plot(right_fitx, yvals, color='white', linewidth=10)
-    plt.gca().invert_yaxis() # to visualize as we do the images
-    y_eval = np.max(yvals)
-    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) \
-                             /np.absolute(2*left_fit[0])
-    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) \
-                                /np.absolute(2*right_fit[0])
-    print(left_curverad, right_curverad)
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    if DEBUG:
+        # Generate x and y values for plotting
+        fity = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        fit_leftx = left_fit[0]*fity**2 + left_fit[1]*fity + left_fit[2]
+        fit_rightx = right_fit[0]*fity**2 + right_fit[1]*fity + right_fit[2]
+
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        plt.imshow(out_img)
+        plt.plot(fit_leftx, fity, color='yellow')
+        plt.plot(fit_rightx, fity, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
 
     print("Warp the detected lane boundaries back onto the original image.")
     result = project_onto(image, warped_image, Minv, 
@@ -246,8 +324,13 @@ def pipeline(cal_vars, src_img):
 
     print("Output visual display of the lane boundaries and numerical estimation ")
     print("of lane curvature and vehicle position.")
-    plt.imshow(result); plt.show()
+    if DEBUG: plt.imshow(result); plt.show()
     return result #TODO: Finish this section
+
+def process_image():
+    # TODO: Implement a generator for video clips?
+    # Would allow for initial sliding window and then optimized version
+    pass
 
 def main():
 
@@ -269,7 +352,7 @@ def main():
     print("Looking at images %s" % images)
     for idx, fname in enumerate(images):
         image = cv2.imread(fname)
-        pipeline(cal_vars, image)
+        pipeline(cal_vars, image, optimize=False)
     # NOTE: save example images from each stage of your pipeline to the output_images folder 
     #       and provide a description of what each image is in your README for the project.
 
